74276111d3ef7f04af1821bc2ef2434e8285f33b
kongil <o_o0122@naver.com>
2022-04-09 17:35:16 +0900

    표준 애너테이션 - Override

---
+++ annotation/src/AnnotationEx1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Parent {
+   void parentMethod() { }
+}
+
+class Child extends Parent {
+    /*
+    @Override
+    void parentmethod() { } // 조상 메서드의 이름을 잘못 적었음 -> 에러
+     */
+}
+
+public class AnnotationEx1 {
+    public static void main(String[] args) {
+
+    }
+}
